a861cb2848ed864fc1fe83e91626314f6f074011
mike18x96 <62031409+mike18x96@users.noreply.github.com>
2022-02-27 11:52:46 +0100

    Feature/add weatherbit api ao  (#5)
    * add implementation for ForecastProvider
    
    (cherry picked from commit 126014b771834c613099476f5e8f7cfded7ac105)

--- src/main/java/com/example/weather_app/controller/Contoller.java
+++ src/main/java/com/example/weather_app/controller/Contoller.java
@@ -1,5 +1,6 @@
 package com.example.weather_app.controller;
 
+import com.example.weather_app.exception.ResourceNotFoundException;
 import com.example.weather_app.model.SurfingSpot;
 import com.example.weather_app.service.SpotProvider;
 import lombok.RequiredArgsConstructor;
@@ -7,17 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
-public class Contoller {
+public class Controller {
 
     private final SpotProvider spotProvider;
 
     @GetMapping("forecast")
-    public SurfingSpot getSurfingSpot(@RequestParam(value = "date") Date date) {
-        return spotProvider.findBestSpot(date).orElseThrow(() -> new IllegalArgumentException("There is no spot"));
+    public SurfingSpot getSurfingSpot(@RequestParam(value = "date") String date) {
+
+        return spotProvider.findBestSpot(LocalDate.parse(date))
+                .orElseThrow(() -> new ResourceNotFoundException("No suitable spots at given date"));
     }
 
 }
