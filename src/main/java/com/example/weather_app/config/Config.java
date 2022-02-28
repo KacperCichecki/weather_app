4e233861b662a0dd8cc344835be8338c1f5e22f7
mike18x96 <mike18x96@gmail.com>
2022-02-28 15:09:10 +0100

    Change springCache to CaffeineCache. findBestSpot() have a cache for 1 hour (for one LocalDate)

--- src/main/java/com/example/weather_app/config/Config.java
+++ src/main/java/com/example/weather_app/config/Config.java
@@ -1,20 +1,32 @@
 package com.example.weather_app.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @EnableCaching
-@Profile("springCache")
+@Profile("caffeineCache")
 public class Config {
 
     @Bean
-    public CacheManager CacheManager() {
-        return new ConcurrentMapCacheManager("surfingSpot");
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("surfingSpot");
+        cacheManager.setCaffeine(caffeineCacheBuilder());
+        return cacheManager;
+    }
+
+    Caffeine<Object, Object> caffeineCacheBuilder() {
+        return Caffeine.newBuilder()
+                .maximumSize(16)
+                .expireAfterAccess(60, TimeUnit.MINUTES)
+                .recordStats();
     }
 
 }
